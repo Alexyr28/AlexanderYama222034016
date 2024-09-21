104b623b6dd3c50e6652f484b35bf2aa6e726d1d
Alexander Yama <alexyr24@gmail.com>
2024-09-21 08:40:02 -0500

    feat: rutas1

--- proyectos Angular/app-rutas/src/app/app.component.ts
+++ proyectos Angular/app-rutas/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from "./modules/home/views/home-page/home-page.component";
+import { AuthPageComponent } from "./modules/auth/views/auth-page/auth-page.component";
+import { SidebarPageComponent } from "./modules/sidebar/views/sidebar-page/sidebar-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomePageComponent, AuthPageComponent, SidebarPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- proyectos Angular/app-rutas/src/app/app.routes.ts
+++ proyectos Angular/app-rutas/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './modules/home/views/home-page/home-page.component';
+import { AuthPageComponent } from './modules/auth/views/auth-page/auth-page.component';
+import { SidebarPageComponent } from './modules/sidebar/views/sidebar-page/sidebar-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '', component: HomePageComponent 
+    },
+    {
+        path:'Auth', component: AuthPageComponent
+    },
+    {
+        path:'sidebar', component: SidebarPageComponent
+    }
+    // Add your routes here
+];

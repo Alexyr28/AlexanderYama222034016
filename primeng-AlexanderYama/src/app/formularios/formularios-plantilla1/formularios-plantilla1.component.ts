62c6d99a0ac9fd834444654b9bb57933b89ab48f
Alexander Yama <alexyr24@gmail.com>
2024-10-19 11:32:26 -0500

    feat: actualizar

--- primeng-AlexanderYama/src/app/formularios/formularios-plantilla1/formularios-plantilla1.component.ts
+++ primeng-AlexanderYama/src/app/formularios/formularios-plantilla1/formularios-plantilla1.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { PasswordModule } from 'primeng/password';
 import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-formularios-plantilla1',
   standalone: true,
-  imports: [PasswordModule, InputTextModule],
+  imports: [PasswordModule, InputTextModule, FormsModule],
   templateUrl: './formularios-plantilla1.component.html',
   styleUrl: './formularios-plantilla1.component.css'
 })

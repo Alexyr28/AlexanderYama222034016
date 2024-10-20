60a47992dc5bba627ca3b212168e4e42e101e771
Alexander Yama <alexyr24@gmail.com>
2024-10-19 21:42:18 -0500

    feat: Final 19/10/2024

--- primeng-AlexanderYama/src/app/app.component.ts
+++ primeng-AlexanderYama/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { FormulariosPlantilla1Component } from "./formularios/formularios-plantilla1/formularios-plantilla1.component";
+import { LoginComponent } from './components/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, FormulariosPlantilla1Component],
+  imports: [RouterOutlet, ButtonModule, FormulariosPlantilla1Component, LoginComponent,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- primeng-AlexanderYama/src/app/components/login/login.component.ts
+++ primeng-AlexanderYama/src/app/components/login/login.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CardModule, ReactiveFormsModule, ButtonModule, RouterModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-
+export class LoginComponent {  
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder){
+    this.loginForm = this.fb.group({
+      usuario: ['',[Validators.required]],
+      password: ['',[Validators.required]]
+    });
+  }
 }

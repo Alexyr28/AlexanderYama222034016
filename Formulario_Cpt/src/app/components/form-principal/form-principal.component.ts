957225e5ce40e26015d7c9bd86cde45e0a8afda9
Alexander Yama <alexyr24@gmail.com>
2024-10-31 17:50:28 -0500

    feat: progreso1.1

--- Formulario_Cpt/src/app/components/form-principal/form-principal.component.ts
+++ Formulario_Cpt/src/app/components/form-principal/form-principal.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-form-principal',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, InputTextModule, CommonModule, InputGroupModule, InputGroupAddonModule],
   templateUrl: './form-principal.component.html',
   styleUrl: './form-principal.component.css'
 })
 export class FormPrincipalComponent {
+  prinForm: FormGroup;
 
+  constructor(private fb: FormBuilder){
+    this.prinForm = this.fb.group({
+      name: ['', Validators.required],
+      apelli: ['', Validators.required],
+      email: ['',[Validators.required, Validators.email]],
+      tel: ['', Validators.required]
+    })
+  }
 }
--- Formulario_Cpt/src/app/components/home/home.component.ts
+++ Formulario_Cpt/src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormPrincipalComponent } from "../form-principal/form-principal.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormPrincipalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
